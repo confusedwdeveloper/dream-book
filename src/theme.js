cdc92bdbe02d7f6c803e8f508d847fe796dfd489
Darshan Datta <darshandatta.10@gmail.com>
2020-07-09 00:34:09 +0530

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+// Create a theme instance.
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#673ab7",
+    },
+    secondary: {
+      main: "#2196f3",
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
